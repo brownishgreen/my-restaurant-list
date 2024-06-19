f8971290e8113c86e9b045c80cb748401261e18e
Yuan WU <jjjjjimyuan@gmail.com>
2024-06-19 21:13:44 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-  res.send(`Hello world`)
+  res.redirect('my-restaurant-list')
+})
+
+app.get('/my-restaurant-list', (req, res) => {
+  res.send('listing restaurant')
+})
+
+app.get('/my-restaurant-list', (req, res) => {
+  const id = req.params.id
+  res.send(`read movie: ${id}`)
 })
 
 app.listen(port, () => {
